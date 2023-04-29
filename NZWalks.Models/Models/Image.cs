685b32bc41304f36a39ea8616c0092593017ca97
Bhav Sagar <bhavsagar5@gmail.com>
2023-04-29 16:04:37 +0530

    - added image

--- NZWalks.Models/Models/Image.cs
+++ NZWalks.Models/Models/Image.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,14 @@ namespace NZWalks.Domain.Models
     public class Image
     {
         public Guid Id { get; set; }
+        [NotMapped]
         public IFormFile File { get; set; }
+
+        public string FileName { get; set; }
+        public string? FileDescription { get; set; }
+        public string FileExtension { get; set; }
+        public long FileSizeInBytes { get; set; }
+        public string FilePath { get; set; }
+
     }
 }
